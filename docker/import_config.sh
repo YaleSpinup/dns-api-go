19de1ce29f54aae5659bb3a2004741c40d702a49
bt353 <brandon.tassone@yale.edu>
2024-04-12 18:20:16 -0400

    Update perms on import_config.sh

--- docker/import_config.sh
+++ docker/import_config.sh
@@ -2,7 +2,7 @@
 # Container runtime configuration script
 # Gets secrets config file from SSM parameter store and uses Deco to substitute parameter values
 # This script expects SSMPATH env variable with the full SSMPATH path to the encrypted config file
-
+k
 if [ -n "$SSMPATH" ]; then
   echo "Getting config file from SSM Parameter Store (${SSMPATH}) ..."
   deco version
