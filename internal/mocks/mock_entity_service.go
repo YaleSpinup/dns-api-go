103815a84f6438ecacea73af87f0dd606adbfdc5
pk556 <prateek.kapoor@yale.edu>
2024-08-19 13:20:11 -0400

    add mock for GetEntities function in mock base service

--- internal/mocks/mock_entity_service.go
+++ internal/mocks/mock_entity_service.go
@@ -8,6 +8,7 @@ import (
 type MockBaseService struct {
 	GetEntityByIDFunc    func(id int, includeHA bool) (*models.Entity, error)
 	DeleteEntityByIDFunc func(id int) error
+	GetEntitiesFunc      func(start int, count int, parentId int, entityType string, includeHA bool) (*[]models.Entity, error)
 }
 
 func (m *MockBaseService) GetEntityByID(id int, includeHA bool) (*models.Entity, error) {
@@ -24,4 +25,12 @@ func (m *MockBaseService) DeleteEntityByID(id int) error {
 	}
 
 	return errors.New("DeleteEntityByID not mocked")
+}
+
+func (m *MockBaseService) GetEntities(start int, count int, parentId int, entityType string, includeHA bool) (*[]models.Entity, error) {
+	if m.GetEntitiesFunc != nil {
+		return m.GetEntitiesFunc(start, count, parentId, entityType, includeHA)
+	}
+
+	return nil, errors.New("GetEntities not mocked")
 }
